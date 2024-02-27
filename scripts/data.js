7ea702f77eb790ed72be42b1bed34fcb60bf4f1e
rir360 <rezowanislamrizvy7@gmail.com>
2024-02-28 02:16:12 +0600

    dataset added to the system

--- scripts/data.js
+++ scripts/data.js
@@ -1,5 +1,40 @@
 const data = [
     {
-        task: ""
+        id: 1,
+        title: "Make a Clock",
+        brief: "Design a JavaScript clock that is functional and as beautiful as you want it to be.",
+        tasks: [
+            {
+                task: "Write JS in a html file that shows today's date on browser.",
+                preview: "image-url.jpeg"
+            },
+            {
+                task: "Make the date and time updates after a second.",
+                preview: "image-url.jpeg"
+            },
+            {
+                task: "Display good morning/noon/afternoon/evening/night text depending upon the time.",
+                preview: "image-url.jpeg"
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Make a Stopwatch",
+        brief: "Design a JavaScript stopwatch from very basic point such as a simple cuonter.",
+        tasks: [
+            {
+                task: "Make a counter first, just show values 1, 2, 3... and so on that keeps incrementing by 1 after each second.",
+                preview: "image-url.jpeg"
+            },
+            {
+                task: "Add three buttons. one for starting the counter and another for stopping it and lastly for resetting it to 0.",
+                preview: "image-url.jpeg"
+            },
+            {
+                task: "Convert  your counter into a stopwatch.",
+                preview: "image-url.jpeg"
+            }
+        ]
     }
 ];
