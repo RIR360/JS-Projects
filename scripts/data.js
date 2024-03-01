a1ed76309597da861ff3435df6c9a71feca895e0
rir360 <rezowanislamrizvy7@gmail.com>
2024-03-02 00:50:53 +0600

    preview images support for tasks

--- scripts/data.js
+++ scripts/data.js
@@ -6,15 +6,15 @@ const data = [
         tasks: [
             {
                 task: "Write JS in a html file that shows today's date on browser.",
-                preview: "image-url.jpeg"
+                preview: "p1-t1.png"
             },
             {
                 task: "Make the date and time updates after a second.",
-                preview: "image-url.jpeg"
+                preview: "p1-t2.png"
             },
             {
                 task: "Display good morning/noon/afternoon/evening/night text depending upon the time.",
-                preview: "image-url.jpeg"
+                preview: "p1-t3.png"
             }
         ]
     },
@@ -25,15 +25,12 @@ const data = [
         tasks: [
             {
                 task: "Make a counter first, just show values 1, 2, 3... and so on that keeps incrementing by 1 after each second.",
-                preview: "image-url.jpeg"
             },
             {
                 task: "Add three buttons. one for starting the counter and another for stopping it and lastly for resetting it to 0.",
-                preview: "image-url.jpeg"
             },
             {
                 task: "Convert  your counter into a stopwatch.",
-                preview: "image-url.jpeg"
             }
         ]
     }
